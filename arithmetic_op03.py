87d0a251cde1ce9b6bfd0bb554c036206618bcba
khazratkulov <khazratkulovdev@gmail.com>
2022-11-28 15:09:18 +0500

    3-task

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -3,3 +3,7 @@
 #Find the reverse of the number and assign it to a variable called 'answer'.
 
 #Print the answer variable
+number = int(input())
+x1 = number%10 #7
+x2 = number//10 #1
+print(x1*10+x2)
